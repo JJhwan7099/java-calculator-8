96abcd20de87f75e78a9f8ec2e0c6a0648e63df0
JJhwan7099 <ghks7099@naver.com>
2025-10-15 20:38:58 +0900

    feat(Application.java): 구분자들로 문자열 분리 함수 구현
    구분자들로 문자열을 분리하는 함수를 구현했습니다.
    - 구분자는 Set에 저장되어 있습니다.
    - 구분자가 나올때 까지 tmpStr에 문자열을 저장하고 구분자가 나오면
      tmpStr를 배열에 추가 및 tmpStr 초기화 하는 로직입니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Set<String> delimiters = new HashSet<>();
+        delimiters.add(",");
+        delimiters.add(":");
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        String[] parts = splitByDelimiters(input, delimiters);
+    }
+
+    private static String[] splitByDelimiters(String str, Set<String> delimiters) {
+        String result = "";
+        String tmpStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if(!delimiters.contains(str.substring(i, i + 1))) {
+                tmpStr += str.substring(i, i + 1);
+                continue;
+            }
+            result += tmpStr + ",";
+            tmpStr = "";
+        }
+        result += tmpStr;
+        return result.split(",");
     }
 }
