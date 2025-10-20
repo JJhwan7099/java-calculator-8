07d428c13775db9bf931482bcdab06e5299b6d1b
JJhwan7099 <ghks7099@naver.com>
2025-10-20 20:55:54 +0900

    refactor(Validator.java): 예외처리 메시지 수정

--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -8,26 +8,26 @@ public class Validator {
     }
 
     public void validateExtractCustomDelimiter(String[] parts) {
-        if(parts.length != 2) {
-            throw new IllegalArgumentException("구분자가 빈값입니다.");
+        if(parts.length < 2) {
+            throw new IllegalArgumentException("커스텀 구분자가 빈값입니다.");
         }
     }
 
     public void validateCustomDelimiter(String delimiter) {
         if (delimiter == null || delimiter.length() != 1) {
-            throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+            throw new IllegalArgumentException("커스텀 구분자는 1자리 문자입니다.");
         }
     }
 
     public void validateParsedNumbers(String[] numbers) {
         for (String number : numbers) {
             if (number == null || number.isEmpty() || !Character.isDigit(number.charAt(0))) {
-                throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+                throw new IllegalArgumentException("계산해야할 문자열 부분이 잘못되었습니다.");
             }
             try {
                 Integer.parseInt(number);
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+                throw new IllegalArgumentException("계산해야할 숫자는 양의 정수입니다.");
             }
         }
     }
